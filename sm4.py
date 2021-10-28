dd3bd00caf38dad8d9a41f9e3d0a524a0c2a83d9
ZelKnow <shenghuang@stu.pku.edu.cn>
2021-10-28 16:00:21 +0800

    init commit

---
+++ sm4.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+"""
+@File          : sm4.py
+@Description   : 
+@Date          : 2021/10/28 15:59:51
+@Author        : ZelKnow
+@Github        : https://github.com/ZelKnow
+"""
+__author__ = "ZelKnow"
+
